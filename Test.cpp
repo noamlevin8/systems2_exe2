4e49336de65a805861bf862254335874a30d959a
Noam Levin <155658672+noamlevin8@users.noreply.github.com>
2024-05-16 21:27:42 +0300

    update

--- Test.cpp
+++ Test.cpp
@@ -180,8 +180,8 @@ TEST_CASE("Test graph equality and inequality")
     CHECK(g1 == g2); // Regular == in undirected
     CHECK(g6 == g7); // Regular == in directed
     CHECK(g1 == g3); // ! g1 > g3 && ! g1 < g3
-    CHECK(g3 != g4); // Different dimensions
-    CHECK(g5 != g3);
+    CHECK(g4 != g3); // g4 > g3
+    CHECK(g5 != g3); // g5 < g3 by edges
 }
 
 TEST_CASE("Test >/< and >=/<=")
@@ -268,6 +268,27 @@ TEST_CASE("Test >/< and >=/<=")
     g8.loadGraph(graph8, 1);
 
     CHECK(g7 > g8);
+
+
+    // Equal which means !< and !>
+
+    ariel::Graph g9;
+    vector<vector<int>> graph9 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g9.loadGraph(graph9, 0);
+
+    ariel::Graph g10;
+    vector<vector<int>> graph10 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g10.loadGraph(graph10, 0);
+
+    CHECK(g9 == g10);
+    CHECK(!(g9 > g10));
+    CHECK(!(g9 < g10));
 }
 
 TEST_CASE("Test ++ and --")
@@ -334,7 +355,6 @@ TEST_CASE("Test graph multiplication")
     g2.loadGraph(graph2, 0);
 
     ariel::Graph g3 = g1 * g2;
-
     vector<vector<int>> expectedGraph = {
         {0, 0, 2},
         {1, 0, 1},
@@ -362,7 +382,6 @@ TEST_CASE("Test graph multiplication")
     g5.loadGraph(graph5, 1);
     
     ariel::Graph g6 = g4 * g5;
-
     CHECK(g6.printGraph() == "[0, -3, 0]\n[8, 0, 0]\n[0, 0, 0]");
 
     g4 * -3;
@@ -466,20 +485,35 @@ TEST_CASE("Test isConnected")
     g2.loadGraph(graph2, 0);
 
     CHECK(Algorithms::isConnected(g1) == "1");
+    
     g1--;
+    // {0, 0, 0},
+    // {0, 0, 0},
+    // {0, 0, 0};
     CHECK(Algorithms::isConnected(g1) == "0"); // We made the edges to be equal to 0 (with --)
 
     CHECK(Algorithms::isConnected(g2) == "1");
+    
     ++g1;
     g2 += g1;
+    // {0, 0, 0},
+    // {0, 0, 0},
+    // {0, 0, 0};
     CHECK(Algorithms::isConnected(g2) == "0"); // We made the edges to be equal to 0 (with +=)
 
     g2 -= g1;
     ariel::Graph g3 = g2 * g1;
+    // {0, -1, -1},
+    // {-1, 0, -1},
+    // {-1, -1, 0};
     CHECK(Algorithms::isConnected(g3) == "1");
 
 }
 
+
+// I will show in this tests that I did not need to change any of the functions in 
+// the Algorithms file because I am only changing the valus of the graphs and 
+// then the functions perform the exact same operation without any problems 
 TEST_CASE("Test shortest path")
 {
     ariel::Graph g1;
@@ -511,29 +545,169 @@ TEST_CASE("Test shortest path")
     g4.loadGraph(graph4, 1);
 
     CHECK(Algorithms::shortestPath(g1, 0, 2) == "0->1->2");
+    
     g1++;
+    // {0, 2, 1},
+    // {2, 0, 2},
+    // {1, 2, 0};
     CHECK(Algorithms::shortestPath(g1, 0, 2) == "0->2"); // We added an edge from 0 to 2
+    
     --g1;
     g1 += g2;
+    // {0, 2, 5},
+    // {2, 0, 2},
+    // {5, 2, 0};
     CHECK(Algorithms::shortestPath(g1, 0, 2) == "0->1->2"); // The edge we added is heavier that 0->1->2
     
     ariel::Graph g5 = g3 * g4;
+    // {0, 15, 1},
+    // {0, 0, 0},
+    // {0, 1, 0};
     CHECK(Algorithms::shortestPath(g5, 0, 1) == "0->2->1");
+    
     g5--;
+    // {0, 14, 0},
+    // {-1, 0, -1},
+    // {-1, 0, 0};
     CHECK(Algorithms::shortestPath(g5, 0, 1) == "0->1");
 }
 
 TEST_CASE("Test isContainsCycle")
 {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph1, 0);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 0, 1},
+        {1, 0, 0},
+        {0, 1, 0}};
+    g2.loadGraph(graph2, 1);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {
+        {0, 1, 1},
+        {2, 0, 1},
+        {0, 3, 0}};
+    g3.loadGraph(graph3, 1);
+
+    CHECK(Algorithms::isContainsCycle(g1) == "0");
+    CHECK(Algorithms::isContainsCycle(g2) == "The cycle is: 0->2->1->0");
+
+    ++g1;
+    // {0, 2, 1},
+    // {2, 0, 2},
+    // {1, 2, 0};
+    CHECK(Algorithms::isContainsCycle(g1) == "The cycle is: 0->1->2->0");
+    
+    g2--;
+    // {0, -1, 0},
+    // {0, 0, -1},
+    // {-1, 0, 0};
+    CHECK(Algorithms::isContainsCycle(g2) == "The cycle is: 0->1->2->0");
+
+    g2++;
+    ariel::Graph g4 = g2 * g3;
+    // {0, 3, 0},
+    // {0, 0, 1},
+    // {2, 0, 0};
+    CHECK(Algorithms::isContainsCycle(g4) == "The cycle is: 0->1->2->0");
+
 
 }
 
 TEST_CASE("Test isBipartite")
 {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph1, 0);
 
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 2},
+        {1, 0, 1},
+        {1, 2, 0}};
+    g2.loadGraph(graph2, 1);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {
+        {0, 1, 1},
+        {2, 0, 1},
+        {0, 3, 0}};
+    g3.loadGraph(graph3, 1);
+
+    CHECK(Algorithms::isBipartite(g1) == "The graph is bipartite: A={0, 2}, B={1}");
+    CHECK(Algorithms::isBipartite(g2) == "0");
+    
+    g1++;
+    // {0, 2, 1},
+    // {2, 0, 2},
+    // {1, 2, 0};
+    CHECK(Algorithms::isBipartite(g1) == "0");
+
+    g2--;
+    // {0, 0, 1},
+    // {0, 0, 0},
+    // {0, 1, 0};
+    CHECK(Algorithms::isBipartite(g2) == "The graph is bipartite: A={0, 1}, B={2}");
+
+    ariel::Graph g4 = g2 * g3;
+    // {0, 3, 0},
+    // {0, 0, 0},
+    // {2, 0, 0};
+    CHECK(Algorithms::isBipartite(g4) == "0");
 }
 
 TEST_CASE("Test negativeCycle")
 {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 0, 0, 1, 0},
+        {-3, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g1.loadGraph(graph1, 1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, -1, 0, 0, 0},
+        {0, 0, -1, 0, 0},
+        {0, 0, 0, 0, -1},
+        {-1, 0, 0, 0, 0},
+        {0, 0, 0, -1, 0}};
+    g2.loadGraph(graph2, 1);
+
+    CHECK(Algorithms::negativeCycle(g1) == "Negative cycle found: 0->3->1->0");
     
+    g1++;
+    // {0, 1, 1, 2, 1}
+    // {-2, 0, 1, 1, 1}
+    // {1, 1, 0, 1, 1}
+    // {1, 2, 1, 0, 1}
+    // {1, 1, 1, 1, 0}}
+    CHECK(Algorithms::negativeCycle(g1) == "Negative cycle found: 0->1->0");
+
+    g1++;
+    // {0, 2, 2, 3, 2}
+    // {-1, 0, 2, 2, 2}
+    // {2, 2, 0, 2, 2}
+    // {2, 3, 2, 0, 2}
+    // {2, 2, 2, 2, 0}}
+    CHECK(Algorithms::negativeCycle(g1) == "No negative cycle found");
+
+    ariel::Graph g3 = g1 * g2;
+    // {0, 0, -2, -2, -2}
+    // {-2, 0, 0, -2, -2}
+    // {-2, -2, 0, -2, 0}
+    // {0, -2, -3, 0, -2}
+    // {-2, -2, -2, 0, 0}
+    CHECK(Algorithms::negativeCycle(g3) == "Negative cycle found: 0->2->3->4->0");
 }
